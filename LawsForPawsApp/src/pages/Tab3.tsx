d36c0fb5114b510eb1d3bcdc5df3e9b8d7399522
theticarcher38 <hunterleegoodman@gmail.com>
2020-11-23 20:53:53 +0000

    Made Changes

--- LawsForPawsApp/src/pages/Tab3.tsx
+++ LawsForPawsApp/src/pages/Tab3.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { IonContent, 
+import { 
+    IonContent, 
     IonHeader, 
     IonPage, 
     IonTitle, 
     IonToolbar 
 } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import './Tab3.css';
 import SettingBtn from '../components/variables/SettingBtn';
 
 const Tab3: React.FC = () => {
